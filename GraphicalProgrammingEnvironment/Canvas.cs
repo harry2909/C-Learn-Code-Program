d91b2d16f98e2b530abaacaafbcb802dde0330d1
Harry <hpreston2909@gmail.com>
2020-10-16 13:34:06 +0100

    Created getter and setter for pen. Created canvas class for graphics. Added comments.

--- GraphicalProgrammingEnvironment/Canvas.cs
+++ GraphicalProgrammingEnvironment/Canvas.cs
@@ -11,7 +11,29 @@ namespace GraphicalProgrammingEnvironment
      // create variable for pen, position and graphics
      // the graphics context is the area of the form to draw on
      private Graphics graph;
-     private Pen Pen;
+     private Pen myPen;
+
+     /// <summary>
+     /// Creating getter and setter for pen
+     /// </summary>
+     public Pen MyPen
+     {
+         set => myPen = new Pen(Color.FromArgb(239, 236, 245));
+         get => myPen;
+         
+     }
+
+     /// <summary>
+     /// Constructor initialises canvas
+     /// </summary>
+     /// <param name="graph">Graphics context of place to draw</param>
+     public Canvas(Graphics graph)
+     {
+         this.graph = graph;
+     }
+     
+     
+
      // the position of the pen when drawing
      private int xPos, yPos;
     }
