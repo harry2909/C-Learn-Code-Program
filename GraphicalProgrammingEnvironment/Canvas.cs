76c2f6cf103214d7b3c160ea7d08cbe76d7d01e4
Harry <hpreston2909@gmail.com>
2020-10-16 13:20:22 +0100

    Added new class to handle drawing. initialised a few variables and added comments

---
+++ GraphicalProgrammingEnvironment/Canvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace GraphicalProgrammingEnvironment
+
+{
+    /// <summary>
+    /// Canvas class holds the data from the form in response to simple commands.
+    /// </summary>
+    class Canvas
+    {
+     // create variable for pen, position and graphics
+     // the graphics context is the area of the form to draw on
+     private Graphics graph;
+     private Pen Pen;
+     // the position of the pen when drawing
+     private int xPos, yPos;
+    }
+}
